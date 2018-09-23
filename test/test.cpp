0c0105ea5a5ac952933db8de17279e6436b61381
KrishnaBhatu <kbhatu@terpmail.umd.edu>
2018-09-22 22:05:45 -0700

    Update unit test

--- test/test.cpp
+++ test/test.cpp
@@ -1,5 +1,15 @@
-#include <gtest/gtest.h>
+#include<gtest/gtest.h>
+#include"pidController.h"
 
-TEST(dummy, should_pass) {
-  EXPECT_EQ(1, 1);
+PidController test1(0.1, 0.01, 0.5);
+
+TEST (PidControllerTest, PIDAlgoTest) {
+  EXPECT_EQ(2.75, test1.calVelocity(60, 50));
+  EXPECT_EQ(1.25, test1.calVelocity(60, 55));
+}
+
+TEST (PidControllerTest, PIDinit) {
+  PidController test2();	
+  EXPECT_EQ(0, test2.errorPrevious);
+  EXPECT_EQ(0, test2.result);
 }
